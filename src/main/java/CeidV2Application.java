10e2591a05175b795995370e7cf46d559bd462e8
Bisheka Weerathunga <weerathungabisheka@gmail.com>
2026-01-08 18:10:20 -0800

    Add files via upload

---
+++ src/main/java/CeidV2Application.java
@@ -0,0 +1,13 @@
+package org.example.ceid_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeidV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeidV2Application.class, args);
+    }
+
+}
